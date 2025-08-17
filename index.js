19e1ba14101791669a8d6abed45569e263cd0df6
Emran khan <emrankhan@Emrans-MacBook-Air.local>
2025-08-17 15:30:17 +0600

    Carousel included and using TMDB API

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 // API KEY 
 const API_KEY = "1d07f6a0a630d302e9910ad9665efa4a"
 const API_URL = `https://api.themoviedb.org/3/trending/movie/week?api_key=${API_KEY}`
-
+// UI
 const cardContainer = document.querySelector(".cards")
+const cards = document.querySelector(".cards")
+const nextBtn = document.querySelector(".next")
+const prevBtn = document.querySelector(".prev")
+
+let scrollAmount = 500
+
 //Fetch API
 fetch(API_URL)
     .then(res => res.json())
@@ -17,4 +23,32 @@ fetch(API_URL)
         })
     })
     .catch(error => console.error("Error fetching data:", error))
+
+// Carousel 
+nextBtn.addEventListener('click', function () {
+    cards.scrollBy({ left: scrollAmount, behavior: 'smooth' })
+})
+
+prevBtn.addEventListener('click', function () {
+    cards.scrollBy({ left: -scrollAmount, behavior: 'smooth' })
+})
+
+cards.addEventListener("scroll", function () {
+    let maxScrollLeft = cards.scrollWidth - cards.clientWidth
+
+    if (cards.scrollLeft <= 0) {
+        prevBtn.style.display = "none"
+    } else {
+        prevBtn.style.display = "block"
+    }
+
+    if (cards.scrollLeft >= maxScrollLeft - 10) {
+        nextBtn.style.display = "none"
+    } else {
+        nextBtn.style.display = "block"
+    }
+})
+
+// Modal
+
 
